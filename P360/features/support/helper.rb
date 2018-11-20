f5f50aef5923a144a733f216c08a4b4f013a7d49
Rafael Fragoso <rafahrfragoso@gmail.com>
2018-11-20 10:40:55 -0300

    Alteração pasta de relatórios e screenshot (log => reports)

--- P360/features/support/helper.rb
+++ P360/features/support/helper.rb
@@ -1,7 +1,7 @@
 module Helper
 
     def tirar_foto(scenario_name, resultado)
-        path_file = "log/screenshots/test_#{resultado}"
+        path_file = "reports/screenshots/test_#{resultado}"
         screenshot = "#{path_file}/#{scenario_name}.png"
         page.save_screenshot(screenshot) 
         embed(screenshot, 'image/png', 'Evidencia')
